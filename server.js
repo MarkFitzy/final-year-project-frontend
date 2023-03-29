decaeac2513e2c88eebb3a657b94fda9e57ef3ce
Mark <markgfitzp@gmail.com>
2023-03-29 23:04:51 +0100

    Undo Heroku Stuff

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/final-year-project-macro-social'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/final-year-project-macro-social/index.html'));});
-app.listen(process.env.PORT || 9090);
