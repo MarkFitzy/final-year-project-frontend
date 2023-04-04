c7805f7cd2ed2377582b3c9845d7bea18039015c
Mark <markgfitzp@gmail.com>
2023-04-04 12:18:10 +0100

    heroku

--- src/app/_services/post-imageservice.ts
+++ src/app/_services/post-imageservice.ts
@@ -9,6 +9,21 @@ export class PostImageService {
 
   constructor(private httpClient: HttpClient) { }
 
+  // public addImage(image: FormData){
+  //   return this.httpClient.post<ImagePost>("http://localhost:9090/addNewImage", image);
+  // }
+
+  // public getAllPosts(searchKeyword: string = "") {
+  //   return this.httpClient.get<ImagePost[]>("http://localhost:9090/getAllPosts?searchKey="+searchKeyword);
+  // }
+
+  // public deletePost(postId: number){
+  //   return this.httpClient.delete("http://localhost:9090/deletePostDetails/" + postId);
+  // }
+
+  // public getPostById(postId: any){
+  //   return this.httpClient.get<ImagePost>("http://localhost:9090/getPostById/" + postId);
+  // }
   public addImage(image: FormData){
     return this.httpClient.post<ImagePost>("https://macro-photography.herokuapp.com/addNewImage", image);
   }
@@ -24,4 +39,8 @@ export class PostImageService {
   public getPostById(postId: any){
     return this.httpClient.get<ImagePost>("https://macro-photography.herokuapp.com/getPostById/" + postId);
   }
+
+  // public getUser() {
+  //   return this.httpClient.delete("http://localhost:9090/deletePostDetails/" + postId);
+  // }
 }
